dd1ec805af0cefcb33856bd39e44c1a224c27409
Rafael Mariotti <rafael.s.mariotti@gmail.com>
2016-09-27 17:19:14 -0300

    setting dockerpy version -  newest version wasnt working

--- preconfig/amazon_linux/chef_config/attributes/oradock.rb
+++ preconfig/amazon_linux/chef_config/attributes/oradock.rb
@@ -8,7 +8,10 @@ default['data']['device']      = '/dev/sdc'
 #python settings - DO NOT CHANGE THIS
 default['pyenv']['version']   = '3.5.1'
 default['pyenv']['root_path'] = '/root/.pyenv'
-default['pyenv']['modules']   = 'boto docopt docker-py'
+default['pyenv']['default_modules']   = 'boto docopt'
+default['pyenv']['docker_module'] = 'docker-py'
+default['pyenv']['docker_module_version'] = '1.9'
+
 
 #yum settings - DO NOT CHANGE THIS
 default['yum']['install'] = 'wget.x86_64','git.x86_64','patch.x86_64','gcc44.x86_64','zlib.x86_64','zlib-devel.x86_64','bzip2.x86_64','bzip2-devel.x86_64','sqlite.x86_64','sqlite-devel.x86_64','openssl.x86_64','openssl-devel.x86_64'
--- preconfig/amazon_linux/chef_config/recipes/pyenv_configure.rb
+++ preconfig/amazon_linux/chef_config/recipes/pyenv_configure.rb
@@ -20,14 +20,20 @@ pyenv_global node['pyenv']['version'] do
   action                        :create
 end
 
-pyenv_script 'pip_install_oradock_dependencies' do
+pyenv_script 'pip_install_upgrade_pip' do
   pyenv_version         node['pyenv']['version']
   root_path                     node['pyenv']['root_path']
   code                          'pip install --upgrade pip'
 end
 
-pyenv_script 'pip_install_oradock_dependencies' do
+pyenv_script 'pip_install_oradock_default_dependencies' do
   pyenv_version		node['pyenv']['version']
   root_path		node['pyenv']['root_path']
   code			'pip install ' + node['pyenv']['modules']
+end
+
+pyenv_script 'pip_install_oradock_dockerpy_dependencie' do
+  pyenv_version     node['pyenv']['version']
+  root_path     node['pyenv']['root_path']
+  code          'pip install ' + node['pyenv']['docker_module'] + '==' + node['pyenv']['docker_module_version']
 end
